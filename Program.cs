649458a5bf112a4ef384967378203d693a56ea82
Sasa23 <Sasa23@SASA>
2019-08-29 16:47:04 -0400

    Sasa

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ism4300myfirstapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is my first console application");
+            Console.WriteLine("I am learning how to publish to github");
+
+        }
+    }
+}
